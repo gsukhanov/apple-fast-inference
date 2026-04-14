75ad01850be1e21490fd76d7800bd5a14cdeacf7
gasukhanov <gregter1999@gmail.com>
2026-04-15 01:06:23 +0300

    added greedy device

--- include/fastinf/core/device.hpp
+++ include/fastinf/core/device.hpp
@@ -3,11 +3,12 @@
 #include <cstdint>
 
 namespace fastinf {
-enum class DeviceLikeType : std::uint8_t { cpu, neon, amx };
+enum class DeviceLikeType : std::uint8_t { cpu, greedy, neon, amx };
 
 template <DeviceLikeType>
 struct Device {};
 using cpu_t = Device<DeviceLikeType::cpu>;
+using greedy_t = Device<DeviceLikeType::cpu>;
 using neon_t = Device<DeviceLikeType::neon>;
 using amx_t = Device<DeviceLikeType::amx>;
 
